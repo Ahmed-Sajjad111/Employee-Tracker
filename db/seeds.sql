6679d7da69a156596a2ea34d05bd35208e8a2955
Ahmed Sajjad <uasajjad11198@outlook.com>
2021-11-06 18:31:07 -0400

    fixed error with seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,8 +6,8 @@ VALUES
 INSERT INTO roles (title, salary, department_id)
 VALUES
     ('Manager', '100000', 1),
-    ('Shift Leader', '75000', 2),
-    ('Associate', '50000', 3);
+    ('Shift Leader', '75000', 1),
+    ('Associate', '50000', 1);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
